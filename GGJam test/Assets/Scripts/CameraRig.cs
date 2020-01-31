78b918dd2064512d3a6297267cfa169ced5220b7
Jeff <droodic.gamedev@gmail.com>
2020-01-31 15:48:50 -0500

    initial push

---
+++ GGJam test/Assets/Scripts/CameraRig.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    [SerializeField] private float smoothTime = 0.2f;
+    [SerializeField] private float _screenRim = 4f;
+    [SerializeField] private float _minimumSize = 6.5f;
+    [SerializeField] private Transform[] _players;
+    private float _zoomSpeed;
+    private Vector3 _smoothVelocity;
+    private Vector3 _camRigPosition;
+    private Camera _camera;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _camera = GetComponentInChildren<Camera>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        GetAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, _camRigPosition, ref _smoothVelocity, smoothTime);
+    }
+
+
+    private void GetAveragePosition()
+    {
+        Vector3 averagePosition = new Vector3();
+        int qtyOfPlayers = 0;
+        foreach (var player in _players)
+        {
+            if (player.gameObject.activeSelf)
+            {
+                averagePosition += player.position;
+                qtyOfPlayers++;
+            }
+        }
+
+        if (qtyOfPlayers != 0)
+            averagePosition /= qtyOfPlayers;
+
+        averagePosition.y = transform.position.y;
+
+        _camRigPosition = averagePosition;
+    }
+
+
+    private void Zoom()
+    {
+        float cameraSize = GetCameraSize();
+        _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, cameraSize, ref _zoomSpeed, smoothTime);
+    }
+
+    private float GetCameraSize()
+    {
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(_camRigPosition);
+
+        float screenSize = 0f;
+
+        foreach (var player in _players)
+        {
+            if (player.gameObject.activeSelf)
+            {
+                Vector3 targetLocalPos = transform.InverseTransformPoint(player.position);
+
+                Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+                screenSize = Mathf.Max(screenSize, Mathf.Abs(desiredPosToTarget.y));
+
+                screenSize = Mathf.Max(screenSize, Mathf.Abs(desiredPosToTarget.x) / _camera.aspect);
+            }
+        }
+
+        screenSize += _screenRim;
+
+        screenSize = Mathf.Max(screenSize, _minimumSize);
+
+        return screenSize;
+    }
+}
---
+++ GGJam test/Assets/Scripts/Movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    private CharacterController _characterController;
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private bool _player2 = false;
+    [SerializeField] private GameObject _arm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        MovePlayer();
+        MoveArm();
+
+    }
+
+    private void MovePlayer()
+    {
+        var newSpeed = _speed;
+        string sprintButton = _player2 ? "Sprint2" : "Sprint";
+        if (Input.GetButton(sprintButton))
+        {
+            newSpeed = _speed * 3.0f;
+        }
+        string xAxis = _player2 ? "Horizontal2" : "Horizontal";
+        string yAxis = _player2 ? "Vertical2" : "Vertical";
+        var x = Input.GetAxis(xAxis) * Time.deltaTime * newSpeed;
+        var y = Input.GetAxis(yAxis) * Time.deltaTime * newSpeed;
+
+        var move = _camera.transform.right * x + _camera.transform.forward * y;
+        move.y = 0.0f;
+        _characterController.Move(move);
+        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+    }
+
+    private void MoveArm()
+    {
+
+        var newSpeed = _speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            newSpeed = _speed * 3.0f;
+        }
+        string xAxis = _player2 ? "HorizontalArm2" : "HorizontalArm";
+        string yAxis = _player2 ? "VerticalArm2" : "VerticalArm";
+        var x = Input.GetAxis(xAxis);
+        var y = Input.GetAxis(yAxis);
+        if (x == 0 && y == 0)
+        {
+            return;
+        }
+        var move = _camera.transform.right * x + _camera.transform.forward * y;
+        move.y = 0.0f;
+        var rotation = Quaternion.LookRotation(move.normalized, Vector3.up);
+        _arm.transform.rotation = rotation;
+    }
+}
