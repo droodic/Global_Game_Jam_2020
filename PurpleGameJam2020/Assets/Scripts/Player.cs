9d2f04639bbf8e5240fb2d5479d7e1580018acbb
Jeff <droodic.gamedev@gmail.com>
2020-01-31 20:48:42 -0500

    finish base energy

--- PurpleGameJam2020/Assets/Scripts/Player.cs
+++ PurpleGameJam2020/Assets/Scripts/Player.cs
@@ -5,31 +5,38 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-
-    [SerializeField] Slider p1slider;
     [SerializeField] float sprintMeter;
 
     bool sprinting;
-
     public float SprintMeter { get => sprintMeter; set => sprintMeter = value; }
+
+    bool player2;
 
     // Start is called before the first frame update
     void Start()
     {
-        p1slider.maxValue = SprintMeter;
+        player2 = this.gameObject.tag == "Player2";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Sprint"))
+        string ButtonName = player2 ? "Sprint2" : "Sprint";
+
+        if (Input.GetButton(ButtonName))
         {
             Sprint();
         }
 
-        if(Input.GetButtonUp("Sprint") || !sprinting)
+        if(Input.GetButtonUp(ButtonName) || !sprinting)
         {
             ClearSprint();
+        }
+
+        //Debug Switch
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            player2 = !player2;
         }
     }
 
@@ -53,10 +60,10 @@ public class Player : MonoBehaviour
         if(SprintMeter < 50)
         {
             SprintMeter += 0.25f;
-          //  p1slider.value = SprintMeter;
+            //p1slider.value = SprintMeter;
             Debug.Log("Sprintmeter" + SprintMeter.ToString());
         }
-        else if (SprintMeter.Equals(p1slider.maxValue))
+        else if (SprintMeter.Equals(50))
         {
             sprinting = true;
         }
