98913bc35a232b44781feada22b992bf60fd53f9
GrisWoldDiablo <griswoldd@hotmail.com>
2020-02-01 17:03:21 -0500

    + Players are asigned their respective mask when spawning

--- PurpleGameJam2020/Assets/_Alex/PlayerManager.cs
+++ PurpleGameJam2020/Assets/_Alex/PlayerManager.cs
@@ -59,13 +59,18 @@ public class PlayerManager : MonoBehaviour
         player.ChangeColor(player.Colors[indexOfPlayer]);
         if (_players.Count == 1)
         {
+            player.tag = "Player";
+            player.gameObject.layer = LayerMask.GetMask("Player1");
             UIManager.Instance.P1 = player;
-            UIManager.Instance.P1.tag = "Player";
+            
+            
         }
         else if (_players.Count == 2)
         {
+            player.tag = "Player2";
+            player.gameObject.layer = LayerMask.GetMask("Player2");
             UIManager.Instance.P2 = player;
-            UIManager.Instance.P2.tag = "Player2";
+
         }
     }
 }
