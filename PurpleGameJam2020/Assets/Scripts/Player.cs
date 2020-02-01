d60b28471ac11250168d6787bb08d8aa96e027d5
Jeff <droodic.gamedev@gmail.com>
2020-01-31 20:16:56 -0500

    started energy system

--- PurpleGameJam2020/Assets/Scripts/Player.cs
+++ PurpleGameJam2020/Assets/Scripts/Player.cs
@@ -1,21 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] int sprintMeter;
-    
+
+    [SerializeField] Slider p1slider;
+    [SerializeField] float sprintMeter;
+
+    bool sprinting;
+
+    public float SprintMeter { get => sprintMeter; set => sprintMeter = value; }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        p1slider.maxValue = SprintMeter;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Sprint"))
+        {
+            Sprint();
+        }
+
+        if(Input.GetButtonUp("Sprint") || !sprinting)
+        {
+            ClearSprint();
+        }
+    }
+
+    void Sprint()
+    {
+        if (!sprinting) sprinting = true; //onlyonce
+
+        if (sprinting && SprintMeter > 0)
+        {
+            SprintMeter--;
+           // p1slider.value = SprintMeter;
+        }
+
+
+        Debug.Log("Sprintmeter" + SprintMeter.ToString());
+
+    }
+    void ClearSprint()
+    {
+        if (sprinting) sprinting = false;
+        if(SprintMeter < 50)
+        {
+            SprintMeter += 0.25f;
+          //  p1slider.value = SprintMeter;
+            Debug.Log("Sprintmeter" + SprintMeter.ToString());
+        }
+        else if (SprintMeter.Equals(p1slider.maxValue))
+        {
+            sprinting = true;
+        }
     }
 }
---
+++ PurpleGameJam2020/Assets/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] Player p1;
+    [SerializeField] Player p2;
+
+    [SerializeField] Slider p1EnergySlider;
+    [SerializeField] Slider p2EnergySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p1EnergySlider.value = p1.SprintMeter;
+        p2EnergySlider.value = p2.SprintMeter;
+    }
+}
