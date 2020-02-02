6193cb63e53fa0950beaa1ec5c9cf86f2c4f4757
Jeff <droodic.gamedev@gmail.com>
2020-02-02 14:04:15 -0500

    victoryscreen

--- PurpleGameJam2020/Assets/Scripts/GameManager.cs
+++ PurpleGameJam2020/Assets/Scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
     }
 
     public void StartGame()
